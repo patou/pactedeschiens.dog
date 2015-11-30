21e87be3973f0f4e6046925b1fffab9f2dc7fdc8
Angelo LIMA <Angelo.LIMA@bio-c-bon.fr>
2015-11-30 17:55:13 +0100

    Avancement blog

--- src/main/webapp/js/route/route.js
+++ src/main/webapp/js/route/route.js
@@ -1,61 +1,21 @@
 app.config(function ($routeProvider) {
     $routeProvider
         .when("/", {
-            controller: function () {
-                window.location.replace('/');
-            }
-        })
-        .when("/index.html", {
-            controller: function () {
-                window.location.replace('/');
-            }
-        })
-        .when("/about.html", {
-            controller: function () {
-                window.location.replace('/about.html');
-            }
-        })
-        .when("/about", {
-            controller: function () {
-                window.location.replace('/about.html');
-            }
-        })
-        .when("/blog.html", {
-            templateUrl: "templates/bloghome.html",
+            templateUrl: "js/templates/bloghome.html",
             controller: "BlogController"
         })
-        .when("/blog.html/", {
-            templateUrl: "templates/bloghome.html",
+        .when("/blog.html", {
+            templateUrl: "js/templates/bloghome.html",
             controller: "BlogController"
         })
         .when("/blog", {
-            templateUrl: "templates/bloghome.html",
+            templateUrl: "js/templates/bloghome.html",
             controller: "BlogController"
         })
         .when("/blog/:id", {
-            templateUrl: "templates/post.html",
+            templateUrl: "js/templates/post.html",
             controller: "BlogController"
 
-        })
-        .when("/contact.html", {
-            controller: function () {
-                window.location.replace('/contact.html');
-            }
-        })
-        .when("/contact", {
-            controller: function () {
-                window.location.replace('/contact.html');
-            }
-        })
-        .when("/partenaires.html", {
-            controller: function () {
-                window.location.replace('/partenaires.html');
-            }
-        })
-        .when("/partenaires", {
-            controller: function () {
-                window.location.replace('/partenaires.html');
-            }
         })
         .otherwise({redirectTo: '/'});
 });
